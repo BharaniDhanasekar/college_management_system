b2adc6814395f90a203b539cda187543186bca4d
BharaniDhanasekar <119785671+BharaniDhanasekar@users.noreply.github.com>
2022-12-11 15:10:54 +0530

    Update settings.py

--- student_management_project/settings.py
+++ student_management_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': 'Chitra',
+        'PASSWORD': 'root',
         'OPTIONS': {
          "init_command": "SET foreign_key_checks = 0;",
     },
